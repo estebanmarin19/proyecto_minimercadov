14ade83f109c2c033be1700ee2e3c5830f46dc24
estebanmarin19 <juanestebanmarin19@gmail.com>
2022-06-22 13:43:41 -0500

    recibido

--- conexion/conexion.js
+++ conexion/conexion.js
@@ -1,8 +1,9 @@
 const mysql=require('mysql');
 module.exports=()=>
 mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'',
-    database:'minimercado'
+    host:'bwo0wc9o8slaphf4va6j-mysql.services.clever-cloud.com',
+    user:'uycaxvayyhk6irho',
+    password:'quO1miD2nwmTE6q7EtOo',
+    database:'bwo0wc9o8slaphf4va6j',
+    port: 3306
 });
